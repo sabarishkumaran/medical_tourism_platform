0a4fc53cae38292b57d2a2da7091655d13186436
Sabarish Kumaran <sabarishkumaranks@gmail.com>
2026-08-22 23:23:47 +0530

    feat: authorization decorators

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -3,7 +3,9 @@ from django.shortcuts import redirect
 
 def hospital_required(view_func):
     def wrapper(request, *args, **kwargs):
-        if request.user.is_authenticated and request.user.role == 'HOSPITAL':
+        if not request.user.is_authenticated:
+            return redirect(f'/accounts/login/?next={request.path}')
+        if request.user.role == 'HOSPITAL':
             hospital = request.user.hospital
             allowed_views = ['hospital_dashboard', 'notify_admin_aged_approval', 'submit_reapproval']
             if hospital.status != 'APPROVED' and view_func.__name__ not in allowed_views:
